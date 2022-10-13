d96c1b4bb8d32b04861f5325f73f6a0b4d22864f
Emilio Gril <emiliogril@gmail.com>
2022-10-13 15:10:15 -0300

    clase 29

---
+++ 29desafio/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+
+//app.use(express.static("public"))
+
+const PORT = parseInt(process.argv[2]) || 8080
+
+app.get("/datos", (req, res) => {
+    console.log(`port: ${PORT} -> Fyh: ${Date.now()}`)
+    res.send(`Servidor express <span style-"color:blueviolet;">(Ngnix)</span> en ${PORT} -
+    <b>PID ${process.pid}</b> - ${new Date().toLocaleString()}`)
+})
+
+app.listen(PORT, err => {
+    if(!err) console.log(`Servidor express escuchando en el puerto ${PORT} - PID WORKER ${process.pid}`)
+})
