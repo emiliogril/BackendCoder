6dd0faf0f05249b450ce934d23a7e0fd67516678
Emilio Gril <emiliogril@gmail.com>
2022-10-23 12:00:44 -0300

    Desafio 32 practicas

--- 32desafio-log2/artillery/server.js
+++ 32desafio-log2/artillery/server.js
@@ -36,20 +36,20 @@ if (modoCluster && cluster.isPrimary) {
         console.log(`PID WORKER ${process.pid}`)
     })
  }
- 
+
 
  function isPrime(num) {
     if ([2, 3].includes(num)) return true;
     else if ([2, 3].some(n => num % n == 0)) return false;
     else {
-    let i = 5, w = 2;
-    while ((i ** 2) <= num) {
-    if (num % i == 0) return false
-    i += w
-    w = 6 - w
-    }
+        let i = 5, w = 2;
+        while ((i ** 2) <= num) {
+            if (num % i == 0) return false
+            i += w
+            w = 6 - w
+        }
     }
     return true
-   }
-   
+ }
+ 
  
