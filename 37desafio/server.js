c2aeba55997bddaa859a66d1cf9dfbf838d16b5c
emilio <emiliogril@gmail.com>
2022-11-14 00:13:52 -0300

    subiendo

---
+++ 37desafio/server.js
@@ -0,0 +1,22 @@
+// nvm-V
+// nvm list available
+//nvm install 16.13.0
+// nvm install 18.1.0
+
+// nvm List
+// nvm use 16.13.0
+// node --verslon
+// npm--version
+
+// para pnpm
+// corepack enable
+// npm install pnpm -g otra opción
+// corepack prepare pnpmolatest --activate
+// npm i nit -y
+// pnpm add express
+//pnpm add -D ts-node-dev
+
+// npm login
+// npm publish
+
+const express = require ("express");
